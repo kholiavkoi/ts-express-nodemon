9c1dfdcf867619e25a86ac684f890886e9ce2a8c
kholiavkoi <76649658+kholiavkoi@users.noreply.github.com>
2023-06-29 23:25:24 +0300

    added express validator

--- src/routes/products-router.ts
+++ src/routes/products-router.ts
@@ -1,14 +1,24 @@
 import { Request, Response, Router } from "express";
 import { productsRepository } from "../repositories/products-repository";
+import { body, validationResult } from "express-validator";
 
 export const productsRouter = Router()
+
+const titleValidation = body('title').trim().isLength({
+    min: 3,
+    max: 10
+}).withMessage('Title length should be from 3 to 10 symbols')
 
 productsRouter.get('/', (req: Request, res: Response) => {
     const foundProducts = productsRepository.findProducts(req.query.title?.toString())
     res.send(foundProducts)
 })
 
-productsRouter.post('/', (req: Request, res: Response) => {
+productsRouter.post('/', titleValidation, (req: Request, res: Response) => {
+    const result = validationResult(req)
+    if (!result.isEmpty()) {
+        return res.send({ errors: result.array() })
+    }
     const newProduct = productsRepository.createProduct(req.body.title)
     res.status(201).send(newProduct)
 })
@@ -22,7 +32,12 @@ productsRouter.get('/:id', (req: Request, res: Response) => {
     }
 })
 
-productsRouter.put('/:id', (req: Request, res: Response) => {
+productsRouter.put('/:id', titleValidation, (req: Request, res: Response) => {
+    const result = validationResult(req)
+    if (!result.isEmpty()) {
+        return res.send({ errors: result.array() })
+    }
+    
     const isUpdated = productsRepository.updateProduct(+req.params.id, req.body.title)
     if (isUpdated) {
         const updatedProduct = productsRepository.getProductById(+req.params.id)
